cdd418ef19301eac3506dc05ed525036b6b11a63
monkalynn813 <byakuya813@hotmail.com>
2019-04-30 21:58:20 -0500

    upload touchscreen code

---
+++ hw9/HW9.X/touchscreen_XPT2046.h
@@ -0,0 +1,14 @@
+#ifndef TOUCHSREEN_H_
+#define TOUCHSREEN_H_
+
+#define SS  LATBbits.LATB9 //
+struct touch_read{
+    unsigned short x;
+    unsigned short y;
+    int z;
+};
+
+void touchscreen_init();
+struct touch_read read_touchpad();
+struct touch_read read_touchinpixel();
+#endif
